cca9f8b595534f3afbbfce2ab04ecb9b210ae751
6RASi <6rasi@proton.me>
2023-07-29 20:53:54 +0100

    Update lspconfig for nix

--- nvim/configs/lspconfig.lua
+++ nvim/configs/lspconfig.lua
@@ -1,17 +1,36 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
+local lsp = require "lspconfig"
+local lsp_status = require "lsp-status"
+local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = vim.tbl_extend('keep', cmp_capabilities, lsp_status.capabilities)
+capabilities = vim.tbl_extend('keep', capabilities, cmp_capabilities)
 
-local lspconfig = require "lspconfig"
+lsp.hls.setup { capabilities = capabilities }
+lsp.pylsp.setup { capabilities = capabilities }
+lsp.bashls.setup { capabilities = capabilities }
+lsp.r_language_server.setup { capabilities = capabilities }
+lsp.html.setup { capabilities = capabilities }
+lsp.eslint.setup { capabilities = capabilities }
+lsp.gdscript.setup { capabilities = capabilities }
+lsp.rnix.setup { capabilities = capabilities }
+lsp.jsonls.setup { capabilities = capabilities }
+lsp.lua_ls.setup {
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' },
+      },
+    },
+  },
+}
+lsp.ltex.setup { capabilities = capabilities }
 
--- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd" }
 
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
-end
 
--- 
--- lspconfig.pyright.setup { blabla}
+-- format buffer on save
+-- neoformat
+vim.cmd [[let g:neoformat_only_msg_on_error = 1]]
+vim.cmd [[augroup fmt
+          autocmd!
+          autocmd BufWritePre * undojoin | Neoformat | lua vim.lsp.buf.format()
+          augroup END]]
