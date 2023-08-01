de92b9789ae49c2d4db281e5bbd18304c7024da3
6RASi <6rasi@proton.me>
2023-08-01 01:47:56 +0100

    Update nvimtree.lua

--- nvimV2/lua/plugins/nvimtree.lua
+++ nvimV2/lua/plugins/nvimtree.lua
@@ -1,80 +1,80 @@
 -- OR setup with some options
 local status, plugin = pcall(require, 'nvim-tree')
 if not status then
-    print('Plugin Error: ', plugin)
-    return
+   print('Plugin Error: ', plugin)
+   return
 end
 
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
+   sort_by = "case_sensitive",
 
--- view
-  view = {
-    adaptive_size = false,
-    side = "left",
-    width = 30,
-    preserve_window_proportions = true,
-  },
-  git = {
-    enable = false,
-    ignore = true,
-  },
-  filesystem_watchers = {
-    enable = true,
-  },
-  actions = {
-    open_file = {
-      resize_window = true,
-    },
-  },
-
-    
--- renderer
-  renderer = {
-    root_folder_label = false,
-    highlight_git = false,
-    highlight_opened_files = "none",
-
-    indent_markers = {
+   -- view
+   view = {
+      adaptive_size = false,
+      side = "left",
+      width = 30,
+      preserve_window_proportions = true,
+   },
+   git = {
       enable = false,
-    },
+      ignore = true,
+   },
+   filesystem_watchers = {
+      enable = true,
+   },
+   actions = {
+      open_file = {
+         resize_window = true,
+      },
+   },
 
-    icons = {
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = false,
+
+   -- renderer
+   renderer = {
+      root_folder_label = false,
+      highlight_git = false,
+      highlight_opened_files = "none",
+
+      indent_markers = {
+         enable = false,
       },
 
-      glyphs = {
-        default = "󰈚",
-        symlink = "",
-        folder = {
-          default = "",
-          empty = "",
-          empty_open = "",
-          open = "",
-          symlink = "",
-          symlink_open = "",
-          arrow_open = "",
-          arrow_closed = "",
-        },
-        git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "",
-          ignored = "◌",
-        },
-      },
+      icons = {
+         show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = false,
+         },
 
--- filters        
-  filters = {
-    dotfiles = true,
-  },
-  },
- },
+         glyphs = {
+            default = "󰈚",
+            symlink = "",
+            folder = {
+               default = "",
+               empty = "",
+               empty_open = "",
+               open = "",
+               symlink = "",
+               symlink_open = "",
+               arrow_open = "",
+               arrow_closed = "",
+            },
+            git = {
+               unstaged = "✗",
+               staged = "✓",
+               unmerged = "",
+               renamed = "➜",
+               untracked = "★",
+               deleted = "",
+               ignored = "◌",
+            },
+         },
+
+         -- filters
+         filters = {
+            dotfiles = true,
+         },
+      },
+   },
 })
