e814c239e4f209020aa295495bff9051752891ed
6RASi <6rasi@proton.me>
2023-08-01 01:52:34 +0100

    Update nvimtree.lua

--- nvimV2/lua/plugins/nvimtree.lua
+++ nvimV2/lua/plugins/nvimtree.lua
@@ -6,75 +6,14 @@ if not status then
 end
 
 require("nvim-tree").setup({
-   sort_by = "case_sensitive",
-
-   -- view
-   view = {
-      adaptive_size = false,
-      side = "left",
-      width = 30,
-      preserve_window_proportions = true,
-   },
-   git = {
-      enable = false,
-      ignore = true,
-   },
-   filesystem_watchers = {
-      enable = true,
-   },
-   actions = {
-      open_file = {
-         resize_window = true,
-      },
-   },
-
-
-   -- renderer
-   renderer = {
-      root_folder_label = false,
-      highlight_git = false,
-      highlight_opened_files = "none",
-
-      indent_markers = {
-         enable = false,
-      },
-
-      icons = {
-         show = {
-            file = true,
-            folder = true,
-            folder_arrow = true,
-            git = false,
-         },
-
-         glyphs = {
-            default = "󰈚",
-            symlink = "",
-            folder = {
-               default = "",
-               empty = "",
-               empty_open = "",
-               open = "",
-               symlink = "",
-               symlink_open = "",
-               arrow_open = "",
-               arrow_closed = "",
-            },
-            git = {
-               unstaged = "✗",
-               staged = "✓",
-               unmerged = "",
-               renamed = "➜",
-               untracked = "★",
-               deleted = "",
-               ignored = "◌",
-            },
-         },
-
-         -- filters
-         filters = {
-            dotfiles = true,
-         },
-      },
-   },
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
 })
