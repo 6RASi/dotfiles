0165c9524794f7cc55198bd01a535cdb5a98dfcf
6RASi <6rasi@proton.me>
2023-08-04 21:04:24 +0100

    oops

--- nvimV2/lua/plugins/treesitter.lua
+++ nvimV2/lua/plugins/treesitter.lua
@@ -4,37 +4,6 @@ if not status then
    return
 end
 plugin.setup {
-   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-   ensure_installed = {
-      "c",
-      "lua",
-      "vim",
-      "python",
-      "html",
-      "htmldjango",
-      "go",
-      "javascript",
-      "typescript",
-      "toml",
-      "css",
-      "gitignore",
-      "gitcommit",
-      "git_rebase",
-      "gitattributes",
-      "cpp",
-      "dockerfile",
-      "c_sharp",
-      "jq",
-      "jsonnet",
-      "kotlin",
-      "rust",
-      "ruby",
-      "solidity",
-      "sql",
-      "yaml",
-      "nix",
-   },
-
    -- Install parsers synchronously (only applied to `ensure_installed`)
    sync_install = false,
 
@@ -50,60 +19,35 @@ plugin.setup {
    indent = { enable = true},
 
    highlight = {
-      -- `false` will disable the whole extension
-      enable = true,
+      enable = true, -- `false` will disable the whole extension
       use_languagetree = true,
       disable = {"css", "html" },
-      local status, plugin = pcall(require,'nvim-treesitter.configs')
-      if not status then
-         print('Plugin Error: ', plugin)
-         return
-      end
-      plugin.setup {
-         -- Install parsers synchronously (only applied to `ensure_installed`)
-         sync_install = false,
-
-         -- Automatically install missing parsers when entering buffer
-         -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-         auto_install = false,
-
-         -- List of parsers to ignore installing (for "all")
-         --ignore_install = { "javascript" },
-
-         ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-         -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-         indent = { enable = true},
-
-         highlight = {
-            enable = true, -- `false` will disable the whole extension
-            use_languagetree = true,
-            disable = {"css", "html" },
-            rainbow = {
-               enable = true,
-               extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-               max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-               colors = {
-                  "#6699CC",
-                  "#5FB3B3",
-                  "#99C794",
-                  "#FAC863",
-                  "#f99157",
-                  "#EC5F67",
-                  "#C594C5",
-               }
-            },
-            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-            -- Using this option may slow down your editor, and you may see some duplicate highlights.
-            -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false,
-
-         },
-      }
-
+      rainbow = {
+         enable = true,
+         extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+         max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+         colors = {
+            "#6699CC",
+            "#5FB3B3",
+            "#99C794",
+            "#FAC863",
+            "#f99157",
+            "#EC5F67",
+            "#C594C5",
+         }
+      },
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
       -- Using this option may slow down your editor, and you may see some duplicate highlights.
       -- Instead of true it can also be a list of languages
       additional_vim_regex_highlighting = false,
 
    },
+}
+
+-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+-- Using this option may slow down your editor, and you may see some duplicate highlights.
+-- Instead of true it can also be a list of languages
+additional_vim_regex_highlighting = false,
+
+},
 }
