fc4d2571f9f35ebfecfba8d392a1176ef661744c
6RASi <6rasi@proton.me>
2023-08-22 21:59:11 +0100

    removed old files

--- nvimV2/lua/plugins/autopairs.lua
+++
@@ -1,9 +0,0 @@
-local status, plugin = pcall(require,'nvim-autopairs')
-if not status then
-    print('Error with plugin: ', plugin)
-    return
-end
-
-require('nvim-autopairs').setup({
-  disable_filetype = { "TelescopePrompt" , "vim" },
-})
