31bc78f39f68a516153594125be9f738a1d446db
6RASi <6rasi@proton.me>
2023-08-22 17:13:59 +0100

    Update lsp.lua

--- nvimV2/lua/plugins/lsp.lua
+++ nvimV2/lua/plugins/lsp.lua
@@ -32,6 +32,13 @@ cmp.setup({
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
       ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+         cmp.select_next_item()
+      else
+         fallback()
+      end
+      end, { "i", "s" }),
       ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
          cmp.select_prev_item()
