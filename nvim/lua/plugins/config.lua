e4cb82989770c01a7fbb5c9a6736f11de3f3c91b
6RASi <6rasi@proton.me>
2023-08-22 21:50:35 +0100

    nvim config.lua

--- nvim/lua/plugins/config.lua
+++ nvim/lua/plugins/config.lua
@@ -1,12 +1,28 @@
-vim.opt.clipboard = "unnamedplus"   -- enable system clipboard
-vim.opt.syntax = "on"               -- syntax highlighting
-vim.opt.number = true               -- line number
-vim.opt.ignorecase = true           -- ignore case when completing file names
-vim.opt.smartcase = true            -- no ignore case when pattern has uppercase
-vim.opt.wrap = true                 -- wrap text
-vim.opt.undofile = true             -- save undo information in a file
-vim.opt.tabstop = 2                 -- spaces per tab
-vim.opt.softtabstop = 2             -- spaces per tab 
-vim.opt.shiftwidth = 2              -- spaces per indent
-vim.opt.numberwidth = 2             -- width of number column
-vim.opt.mouse = "a"                 -- enable mouse 
+local opt = vim.opt
+
+-- other
+opt.clipboard = "unnamedplus"   -- enable system clipboard
+opt.termguicolors = true        -- enables RGB colors
+opt.wrap = true                 -- wrap text
+opt.mouse = "a"                 -- enable mouse
+
+
+-- files 
+opt.ignorecase = true           -- ignore case when completing file names
+opt.smartcase = true            -- no ignore case when pattern has uppercase
+opt.undofile = true             -- save undo information in a file
+
+-- numbers
+opt.number = true               -- line number
+opt.numberwidth = 2             -- width of number column
+opt.ruler = false               -- disable ruler
+
+-- indents
+opt.smartindent = true          -- smart autoindenting for C programs
+opt.tabstop = 2                 -- spaces per tab
+opt.softtabstop = 2             -- spaces per tab 
+opt.shiftwidth = 2              -- spaces per indent
+
+
+
+
