694cfaa358f00cf7ee5b7754aa5faa1af1c4d314
6RASi <6rasi@proton.me>
2023-08-24 14:05:42 +0100

    dashboard fix

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,49 +1,49 @@
 local status, db = pcall(require, "dashboard")
 if (not status) then
-    return
+   return
 end
 
 
- local custom_header = {
+local custom_header = {
 
-    "   ▄▀▀▀▀▄       ▄▀▀▄ ▀▄      ▄▀▀█▀▄   	",
-    "  █      █     █  █ █ █     █   █  █  	",
-    "  █      █     ▐  █  ▀█     ▐   █  ▐  	",
-    "  ▀▄    ▄▀       █   █          █     	",
-    "    ▀▀▀▀       ▄▀   █        ▄▀▀▀▀▀▄  	",
-    "               █    ▐       █       █ 	",
-    "               ▐            ▐       ▐  ",
-     "                   										",
+   "   ▄▀▀▀▀▄       ▄▀▀▄ ▀▄      ▄▀▀█▀▄   	",
+   "  █      █     █  █ █ █     █   █  █  	",
+   "  █      █     ▐  █  ▀█     ▐   █  ▐  	",
+   "  ▀▄    ▄▀       █   █          █     	",
+   "    ▀▀▀▀       ▄▀   █        ▄▀▀▀▀▀▄  	",
+   "               █    ▐       █       █ 	",
+   "               ▐            ▐       ▐   ",
+   "                   										",
 }
 
--- [[ custom_header = {
- ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
- ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
- ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
- ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
- ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
- ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+--[[
+local custom_header = {
+   ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+   ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+   ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+   ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+   ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+   ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
 } ]]
 
-}
 local custom_center = {
-    {icon = "  ", icon_hl = 'Title', desc = "File Oldfiles ", desc_hl = 'String', key = "SPC f o", key_hl = 'Number', action = "Telescope oldfiles theme=dropdown"},
-    {icon = "  ", icon_hl = 'Title', desc = "Find File     ", desc_hl = 'String', key = "SPC f f", key_hl = 'Number', action = "Telescope find_files find_command=rg,--hidden,--files"},
-    {icon = "  ", icon_hl = 'Title', desc = "File Browser  ", desc_hl = 'String', key = "SPC f b", key_hl = 'Number', action = "Telescope file_browser theme=dropdown previewer=false"},
-    {icon = "  ", icon_hl = 'Title', desc = "Find Word     ", desc_hl = 'String', key = "SPC f w", key_hl = 'Number', action = "Telescope live_grep"},
-    {icon = " ",  icon_hl = 'Title', desc = "Git summary   ", desc_hl = "string", key = "SPC g s", key_hl = "Number", action = ":Git | :only<CR>"},
+   {icon = "  ", icon_hl = 'Title', desc = "File Oldfiles ", desc_hl = 'String', key = "SPC f o", key_hl = 'Number', action = "Telescope oldfiles theme=dropdown"},
+   {icon = "  ", icon_hl = 'Title', desc = "Find File     ", desc_hl = 'String', key = "SPC f f", key_hl = 'Number', action = "Telescope find_files find_command=rg,--hidden,--files"},
+   {icon = "  ", icon_hl = 'Title', desc = "File Browser  ", desc_hl = 'String', key = "SPC f b", key_hl = 'Number', action = "Telescope file_browser theme=dropdown previewer=false"},
+   {icon = "  ", icon_hl = 'Title', desc = "Find Word     ", desc_hl = 'String', key = "SPC f w", key_hl = 'Number', action = "Telescope live_grep"},
+   {icon = " ",  icon_hl = 'Title', desc = "Git summary   ", desc_hl = "string", key = "SPC g s", key_hl = "Number", action = ":Git | :only<CR>"},
 }
 local custom_footer = function()
-    local footer = {'','','','','','Is it time for magic? 🎩'}
-    -- print(footer)
-    return footer
+local footer = {'','','','','','Is it time for magic? 🎩'}
+-- print(footer)
+return footer
 end
 
 db.setup({
-    theme = 'ayu',
-    config = {
-        header = custom_header,
-        center = custom_center,
-        footer = custom_footer,
-    }
+theme = 'ayu',
+config = {
+   header = custom_header,
+   center = custom_center,
+   footer = custom_footer,
+}
 })
